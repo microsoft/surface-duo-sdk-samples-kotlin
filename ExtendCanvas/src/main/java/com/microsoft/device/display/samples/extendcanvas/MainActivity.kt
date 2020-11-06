c79425e0e9fb6c7efe8077bbe0f7b2b5beb28a09
Joy Liu <joyliu1@microsoft.com>
2020-11-05 16:12:03 -0800

    Update the top bar menu

--- ExtendCanvas/src/main/java/com/microsoft/device/display/samples/extendcanvas/MainActivity.kt
+++ ExtendCanvas/src/main/java/com/microsoft/device/display/samples/extendcanvas/MainActivity.kt
@@ -19,6 +19,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.menu);
     }
 
     @SuppressLint("ClickableViewAccessibility")
