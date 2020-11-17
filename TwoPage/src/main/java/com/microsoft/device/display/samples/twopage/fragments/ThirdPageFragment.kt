2c414926ed5d7e9f1c49eebe923ff96ac83a787b
Joy Liu <joyl1216@hotmail.com>
2020-11-16 17:43:12 -0800

    Fix code style

--- TwoPage/src/main/java/com/microsoft/device/display/samples/twopage/fragments/ThirdPageFragment.kt
+++ TwoPage/src/main/java/com/microsoft/device/display/samples/twopage/fragments/ThirdPageFragment.kt
@@ -1,15 +1,16 @@
 package com.microsoft.device.display.samples.twopage.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.microsoft.device.display.samples.twopage.R
 
 class ThirdPageFragment : Fragment() {
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
