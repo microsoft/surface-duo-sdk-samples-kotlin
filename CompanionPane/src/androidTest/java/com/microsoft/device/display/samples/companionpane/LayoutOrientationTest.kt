23f7e69f357f3805843fed6a30c438fdf350ba59
EUROPE\bimiron <bimiron@microsoft.com>
2020-12-09 14:43:45 +0200

    Migrated CompanionPane sample to use the new dual screen SDK version and updated UI tests

--- CompanionPane/src/androidTest/java/com/microsoft/device/display/samples/companionpane/LayoutOrientationTest.kt
+++ CompanionPane/src/androidTest/java/com/microsoft/device/display/samples/companionpane/LayoutOrientationTest.kt
@@ -27,7 +27,7 @@ class LayoutOrientationTest {
     private val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
 
     @get:Rule
-    val activityRule = ActivityTestRule<MainActivity>(MainActivity::class.java)
+    val activityRule = ActivityTestRule(MainActivity::class.java)
 
     @After
     fun resetOrientation() {
@@ -37,7 +37,7 @@ class LayoutOrientationTest {
 
     @Test
     fun shouldFindLayout_whenInSinglePortrait() {
-        onView(withId(R.id.single_screen_container_id)).check(matches(isDisplayed()))
+        onView(withId(R.id.first_container_id)).check(matches(isDisplayed()))
         onView(withId(R.id.single_screen_layout_port)).check(matches(isDisplayed()))
     }
 
@@ -45,7 +45,7 @@ class LayoutOrientationTest {
     fun shouldFindLayout_whenInSingleLandscape() {
         rotateDevice()
 
-        onView(withId(R.id.single_screen_container_id)).check(matches(isDisplayed()))
+        onView(withId(R.id.first_container_id)).check(matches(isDisplayed()))
         onView(withId(R.id.single_screen_layout_land)).check(matches(isDisplayed()))
     }
 
@@ -53,11 +53,11 @@ class LayoutOrientationTest {
     fun shouldFindLayouts_whenInDoublePortrait() {
         spanApplication()
 
-        onView(withId(R.id.dual_screen_start_container_id)).check(matches(isDisplayed()))
-        onView(withId(R.id.picture_dual_layout)).check(matches(isDisplayed()))
+        onView(withId(R.id.first_container_id)).check(matches(isDisplayed()))
+        onView(withId(R.id.dual_screen_layout_land_start)).check(matches(isDisplayed()))
 
-        onView(withId(R.id.dual_screen_end_container_id)).check(matches(isDisplayed()))
-        onView(withId(R.id.tools_dual_layout_land)).check(matches(isDisplayed()))
+        onView(withId(R.id.second_container_id)).check(matches(isDisplayed()))
+        onView(withId(R.id.dual_screen_layout_land_end)).check(matches(isDisplayed()))
     }
 
     @Test
@@ -65,31 +65,15 @@ class LayoutOrientationTest {
         spanApplication()
         rotateDevice()
 
-        onView(withId(R.id.dual_screen_start_container_id)).check(matches(isDisplayed()))
-        onView(withId(R.id.picture_dual_layout)).check(matches(isDisplayed()))
+        onView(withId(R.id.first_container_id)).check(matches(isDisplayed()))
+        onView(withId(R.id.dual_screen_layout_port_start)).check(matches(isDisplayed()))
 
-        onView(withId(R.id.dual_screen_end_container_id)).check(matches(isDisplayed()))
-        onView(withId(R.id.tools_dual_layout_port)).check(matches(isDisplayed()))
-    }
-
-    @Test
-    fun shouldFindLayouts_whenSpanningInDoubleLandscape() {
-        rotateDevice()
-        spanLandscapeApplication()
-
-        onView(withId(R.id.dual_screen_start_container_id)).check(matches(isDisplayed()))
-        onView(withId(R.id.picture_dual_layout)).check(matches(isDisplayed()))
-
-        onView(withId(R.id.dual_screen_end_container_id)).check(matches(isDisplayed()))
-        onView(withId(R.id.tools_dual_layout_port)).check(matches(isDisplayed()))
+        onView(withId(R.id.second_container_id)).check(matches(isDisplayed()))
+        onView(withId(R.id.dual_screen_layout_port_end)).check(matches(isDisplayed()))
     }
 
     private fun spanApplication() {
         device.swipe(675, 1780, 1350, 900, 400)
-    }
-
-    private fun spanLandscapeApplication() {
-        device.swipe(1780, 2100, 1350, 1500, 400)
     }
 
     private fun rotateDevice() {
