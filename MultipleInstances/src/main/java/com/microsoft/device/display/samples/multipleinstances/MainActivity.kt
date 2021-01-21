11488908a590c7d91fcdb29be2ee580b8ad28496
Andrei Cirja <72752597+ancirja-m@users.noreply.github.com>
2021-01-21 13:41:27 +0200

    Code refactory and UI improvements for MultipleInstances sample (#28)
    Code refactory and UI improvements for MultipleInstances sample

--- MultipleInstances/src/main/java/com/microsoft/device/display/samples/multipleinstances/MainActivity.kt
+++ MultipleInstances/src/main/java/com/microsoft/device/display/samples/multipleinstances/MainActivity.kt
@@ -1,51 +1,23 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ *
+ */
+
 package com.microsoft.device.display.samples.multipleinstances
 
-import android.content.Intent
-import android.content.pm.ShortcutInfo
-import android.content.pm.ShortcutManager
-import android.net.Uri
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
-
+/**
+ * Implementation for [BaseActivity]
+ */
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         supportActionBar?.title = getString(R.string.main_shortcut_label)
-        addShortcuts()
     }
 
-    private fun addShortcuts() {
-        val intent = Intent(
-            Intent.ACTION_MAIN, Uri.EMPTY,
-            this,
-            MainActivity::class.java
-        )
-        intent.addFlags(Intent.FLAG_ACTIVITY_MULTIPLE_TASK or Intent.FLAG_ACTIVITY_SINGLE_TOP)
+    override fun getFirstScreenText(): String = getString(R.string.main_activity_text)
 
-        val intent2 = Intent(
-            Intent.ACTION_MAIN,
-            Uri.EMPTY,
-            this,
-            SecondActivity::class.java
-        )
-        intent.addFlags(Intent.FLAG_ACTIVITY_MULTIPLE_TASK or Intent.FLAG_ACTIVITY_SINGLE_TOP)
-
-        val shortcutManager = getSystemService<ShortcutManager>(ShortcutManager::class.java)
-        val shortcut = ShortcutInfo.Builder(this, "id1")
-            .setShortLabel(getString(R.string.main_shortcut_label))
-            .setLongLabel(getString(R.string.main_shortcut_label))
-            .setIntent(intent)
-            .build()
-        val shortcut2 = ShortcutInfo.Builder(this, "id2")
-            .setShortLabel(getString(R.string.second_shortcut_label))
-            .setLongLabel(getString(R.string.second_shortcut_label))
-            .setIntent(intent2)
-            .build()
-
-        if (shortcutManager != null) {
-            shortcutManager.dynamicShortcuts = listOf(shortcut, shortcut2)
-        }
-    }
+    override fun getSecondScreenText(): String = getString(R.string.main_activity_text)
 }
