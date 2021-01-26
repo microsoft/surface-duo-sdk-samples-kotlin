d868ab987fa7dbd18db749155fb4982e2f370d4f
Andrei Cirja <72752597+ancirja-m@users.noreply.github.com>
2021-01-26 11:46:39 +0200

    Code refactory and UI improvements for Extended Canvas sample (#33)
    Code refactory and UI improvements for Extended Canvas sample

--- ExtendCanvas/src/main/java/com/microsoft/device/display/samples/extendcanvas/ExtendedCanvasActivity.kt
+++ ExtendCanvas/src/main/java/com/microsoft/device/display/samples/extendcanvas/ExtendedCanvasActivity.kt
@@ -3,23 +3,30 @@
  * Licensed under the MIT License.
  *   
  */
+
 package com.microsoft.device.display.samples.extendcanvas
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
 import com.microsoft.device.display.samples.extend.R
 
-class MainActivity : AppCompatActivity() {
+class ExtendedCanvasActivity : AppCompatActivity() {
+    companion object {
+        const val GOOGLE_MAPS_URL = "https://www.google.com/maps/@?api=1&map_action=map&center=47.652822, -122.134596&zoom=12"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.setHomeAsUpIndicator(R.drawable.menu)
+        setContentView(R.layout.activity_extended_canvas)
+        setupWebView()
+    }
 
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun setupWebView() {
         val webView = findViewById<WebView>(R.id.webView)
         webView.settings.javaScriptEnabled = true
-        webView.loadUrl("https://www.google.com/maps/@?api=1&map_action=map&center=47.652822, -122.134596&zoom=12")
+        webView.loadUrl(GOOGLE_MAPS_URL)
     }
 }
