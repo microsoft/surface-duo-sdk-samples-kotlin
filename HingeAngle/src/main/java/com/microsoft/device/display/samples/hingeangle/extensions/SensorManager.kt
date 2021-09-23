bbfab5ba40176da883a796292f1656a2f11bedad
Sorin Albu <74547562+soalb-m@users.noreply.github.com>
2021-09-23 17:33:54 +0300

    Fixed Hinge Sensor on A11 emulators (#53)

--- HingeAngle/src/main/java/com/microsoft/device/display/samples/hingeangle/extensions/SensorManager.kt
+++ HingeAngle/src/main/java/com/microsoft/device/display/samples/hingeangle/extensions/SensorManager.kt
@@ -9,12 +9,14 @@ package com.microsoft.device.display.samples.hingeangle.extensions
 import android.hardware.Sensor
 import android.hardware.SensorManager
 
-private const val HINGE_ANGLE_SENSOR_NAME = "Hinge Angle"
+private const val A10_HINGE_ANGLE_SENSOR_NAME = "Hinge Angle"
+private const val A11_HINGE_ANGLE_SENSOR_NAME = "Goldfish hinge sensor"
 
 /**
  * Finds and returns the hinge angle sensor, or [null] if doesn't exists
  */
 val SensorManager?.hingeAngleSensor: Sensor?
     get() = this?.getSensorList(Sensor.TYPE_ALL)?.firstOrNull {
-        it.name.contains(HINGE_ANGLE_SENSOR_NAME)
+        it.name.contains(A10_HINGE_ANGLE_SENSOR_NAME) ||
+            it.name.contains(A11_HINGE_ANGLE_SENSOR_NAME)
     }
