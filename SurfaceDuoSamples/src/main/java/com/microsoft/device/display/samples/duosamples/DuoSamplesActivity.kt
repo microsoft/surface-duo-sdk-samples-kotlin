e23ca9ad2df134f8459d01c5c54d865bdca2ac53
Sorin Albu <soalb@microsoft.com>
2022-02-07 10:12:53 +0200

    Added Navigation Component

--- SurfaceDuoSamples/src/main/java/com/microsoft/device/display/samples/duosamples/DuoSamplesActivity.kt
+++ SurfaceDuoSamples/src/main/java/com/microsoft/device/display/samples/duosamples/DuoSamplesActivity.kt
@@ -8,10 +8,14 @@ package com.microsoft.device.display.samples.duosamples
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.FoldableNavigation
 import com.microsoft.device.display.samples.duosamples.databinding.ActivityDuoSamplesBinding
+import com.microsoft.device.display.samples.duosamples.navigation.MainNavigator
+import com.microsoft.device.display.samples.duosamples.navigation.MainNavigatorHost
 
-class DuoSamplesActivity : AppCompatActivity() {
+class DuoSamplesActivity : AppCompatActivity(), MainNavigatorHost {
     private lateinit var binding: ActivityDuoSamplesBinding
+    private var navigator: MainNavigator = MainNavigator()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,5 +25,21 @@ class DuoSamplesActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(false)
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_arrow_back)
         supportActionBar?.title = getString(R.string.surface_duo_samples_app_title)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        FoldableNavigation.findNavController(this, R.id.launch_nav_host_fragment).let {
+            navigator.bind(it)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        navigator.unbind()
+    }
+
+    override fun getMainNavigator(): MainNavigator {
+        return navigator
     }
 }
