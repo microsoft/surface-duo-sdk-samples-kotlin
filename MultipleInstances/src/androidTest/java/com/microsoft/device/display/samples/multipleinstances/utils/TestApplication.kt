526279048463509e5b19522bc5b3346fce8ac6d7
Sorin Albu <74547562+soalb-m@users.noreply.github.com>
2022-02-18 08:59:57 +0200

    Removed ScreenManager dependency (#59)

--- MultipleInstances/src/androidTest/java/com/microsoft/device/display/samples/multipleinstances/utils/TestApplication.kt
+++ MultipleInstances/src/androidTest/java/com/microsoft/device/display/samples/multipleinstances/utils/TestApplication.kt
@@ -8,12 +8,10 @@ package com.microsoft.device.display.samples.multipleinstances.utils
 
 import android.app.Application
 import com.microsoft.device.display.samples.multipleinstances.createShortcuts
-import com.microsoft.device.dualscreen.ScreenManagerProvider
 
 class TestApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        ScreenManagerProvider.init(this)
         createShortcuts()
     }
 }
