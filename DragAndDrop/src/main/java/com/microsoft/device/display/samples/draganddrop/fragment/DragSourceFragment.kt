78be5ff23a32c916f82b3373297e493d5bafcd8c
Sorin Albu <74547562+soalb-m@users.noreply.github.com>
2022-05-25 08:01:59 +0300

    [Drag and drop] Update UI and  API, fix bugs and keep states. (#68)
    * Update UI and  API, fix bugs and keep states.

--- DragAndDrop/src/main/java/com/microsoft/device/display/samples/draganddrop/fragment/DragSourceFragment.kt
+++ DragAndDrop/src/main/java/com/microsoft/device/display/samples/draganddrop/fragment/DragSourceFragment.kt
@@ -7,25 +7,34 @@
 package com.microsoft.device.display.samples.draganddrop.fragment
 
 import android.content.ClipData
+import android.content.ClipDescription
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.DRAG_FLAG_GLOBAL
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.FileProvider
+import androidx.core.graphics.drawable.toBitmap
+import androidx.core.view.DragStartHelper
 import androidx.fragment.app.Fragment
-import com.microsoft.device.display.samples.draganddrop.R
 import com.microsoft.device.display.samples.draganddrop.databinding.DragAndDropSourceLayoutBinding
-import com.microsoft.device.display.samples.draganddrop.utils.mimeType
-import com.microsoft.device.display.samples.draganddrop.utils.value
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 
 /**
  * The Fragment implementation containing the drag source zone
  */
-class DragSourceFragment : Fragment(), View.OnLongClickListener {
+class DragSourceFragment : Fragment() {
 
     companion object {
         fun newInstance(): DragSourceFragment = DragSourceFragment()
+
+        private const val DRAG_FOLDER_NAME = "images"
+        private const val DRAG_FILE_NAME = "dragged_image.png"
     }
 
     private lateinit var binding: DragAndDropSourceLayoutBinding
@@ -34,42 +43,62 @@ class DragSourceFragment : Fragment(), View.OnLongClickListener {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = DragAndDropSourceLayoutBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        bindUI(view)
+        prepareDragListeners()
     }
 
     /**
      * Binds all UI components
      */
-    private fun bindUI(view: View) {
-        val dragTextView = view.findViewById<TextView>(R.id.drag_text_view)
-        val dragImageView = view.findViewById<ImageView>(R.id.drag_image_view)
-        dragTextView.tag = "text_view"
-        dragImageView.tag = "image_view"
-        dragTextView.setOnLongClickListener(this)
-        dragImageView.setOnLongClickListener(this)
+    private fun prepareDragListeners() {
+        DragStartHelper(binding.dragTextView) { targetView, _ ->
+            val text = (targetView as TextView).text
+            val dragClipData = ClipData.newPlainText(ClipDescription.MIMETYPE_TEXT_PLAIN, text)
+            val dragShadowBuilder = View.DragShadowBuilder(targetView)
+            targetView.startDragAndDrop(dragClipData, dragShadowBuilder, null, DRAG_FLAG_GLOBAL)
+        }.attach()
+
+        DragStartHelper(binding.dragImageView) { view, _ ->
+            val folder = File(requireContext().filesDir, DRAG_FOLDER_NAME)
+            if (!folder.exists()) {
+                folder.mkdir()
+            }
+            val imageFile = File(folder, DRAG_FILE_NAME)
+
+            ByteArrayOutputStream().use { bos ->
+                (view as ImageView).drawable.toBitmap()
+                    .compress(Bitmap.CompressFormat.PNG, 0, bos)
+                FileOutputStream(imageFile).use { fos ->
+                    fos.write(bos.toByteArray())
+                    fos.flush()
+                }
+            }
+
+            val imageUri = FileProvider.getUriForFile(
+                requireContext(),
+                getFileProviderAuthority(),
+                imageFile
+            )
+            val dragClipData =
+                ClipData.newUri(requireContext().contentResolver, DRAG_FOLDER_NAME, imageUri)
+            val dragShadow = View.DragShadowBuilder(view)
+
+            view.startDragAndDrop(
+                dragClipData,
+                dragShadow,
+                null,
+                // Need to Use the DRAG_FLAG_GLOBAL_URI_READ to allow other apps to read from our
+                // content provider. Without it, other apps won't receive the drag events.
+                DRAG_FLAG_GLOBAL.or(View.DRAG_FLAG_GLOBAL_URI_READ)
+            )
+        }.attach()
     }
 
-    override fun onLongClick(view: View): Boolean {
-        startDragAndDropForView(view)
-        return true
-    }
-
-    /**
-     * Starts the drag and drop action for the given [View]
-     *
-     * @param view The drag source
-     */
-    private fun startDragAndDropForView(view: View) {
-        val clipDataItem = ClipData.Item(view.tag.toString())
-        val mimeTypes = arrayOf(view.mimeType?.value)
-        val clipData = ClipData(view.tag.toString(), mimeTypes, clipDataItem)
-        view.startDragAndDrop(clipData, View.DragShadowBuilder(view), view, 0)
-    }
+    private fun getFileProviderAuthority() = requireContext().packageName + ".images"
 }
